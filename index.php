90622c4618c23cafcb8e2373800acf3ad7dfa3a5
adnane <adnanebaraka546@gmail.com>
2023-11-24 22:12:19 +0000

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- index.php -->
+<?php  
+require_once './header.php';
+
+?>
+<section>
+<?php include './nav.php'; ?>
+<div class="">
+
+</div>
+
+</section>
+
+<?php 
+require_once './footer.php';
+
+?>
