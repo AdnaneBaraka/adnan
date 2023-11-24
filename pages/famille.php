3c316fbd83dddf48ccd627a926c53091db64d6b3
Otmane <samdev.br@gmail.com>
2023-11-25 00:07:46 +0100

    side bar

---
+++ pages/famille.php
@@ -0,0 +1,21 @@
+<?php 
+
+$title = 'Famille';
+
+include 'header.php'; 
+
+?>
+
+<div class="app-container">
+    <?php 
+        include 'side-bar.php';
+    ?>
+    <div class="content container">
+    </div>
+</div>
+
+<?php
+
+include 'footer.php';
+
+?>
--- side-bar.php
+++ side-bar.php
@@ -1,4 +1,10 @@
-<div class="side-bar">
+<div class="bg-light side-bar border-end">
     <img src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png" class="logo" alt="">
-    
+    <ul class="nav">
+        <li class="nav-item"><a class="nav-link text-dark" href="client.php">Client</a></li> 
+        <li class="nav-item"><a class="nav-link text-dark" href="caissier.php">Caissier</a></li>
+        <li class="nav-item"><a class="nav-link text-dark" href="article.php">Article</a></li> 
+        <li class="nav-item"><a class="nav-link text-dark" href="pages/famille.php">Famille</a></li> 
+        <li class="nav-item"><a class="nav-link text-dark" href="bonlivraison.php">BonLivraison</a></li>  
+    </ul>
 </div>
