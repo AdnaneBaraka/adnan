5a4186527e9dd43d9c0cec8cc2bf7469cdc0b785
Otmane <samdev.br@gmail.com>
2023-12-29 00:36:27 +0100

    facture

--- facture.php
+++ facture.php
@@ -2,129 +2,88 @@
 $title = 'bonlivraison';
 include 'header.php';
 include 'dbconnect.php';
-?>
 
-<?php
-$bonLivraisonRow = null;
-$statementDetailBl = null;
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-
-if ($_SERVER['REQUEST_METHOD'] === "GET" && isset($_GET['id'])) {
-    $id = $_GET['id'];
-    
-    // Query for BonLivraison
-    $queryBonLivraison = "SELECT * FROM BonLivraison WHERE id = ?";
-    $statementBonLivraison = mysqli_prepare($conn, $queryBonLivraison);
-    mysqli_stmt_bind_param($statementBonLivraison, 'i', $id);
-    mysqli_stmt_execute($statementBonLivraison);
-    $resultBonLivraison = mysqli_stmt_get_result($statementBonLivraison);
-    $bonLivraisonRow = mysqli_fetch_assoc($resultBonLivraison);
-
-    // Query for DetailBL
-    $queryDetailBl = "SELECT * FROM detail_bl WHERE bl_id = ?";
-    $statementDetailBl = mysqli_prepare($conn, $queryDetailBl);
-    mysqli_stmt_bind_param($statementDetailBl, 'i', $id);
-    mysqli_stmt_execute($statementDetailBl);
-    $resultDetailBl = mysqli_stmt_get_result($statementDetailBl);
-
-    if ($bonLivraisonRow) {
-        $client_id = $bonLivraisonRow['client_id'];
-        $caissier_id = $bonLivraisonRow['caissier_id'];
-
-        // Query for Client
-        $queryClient = "SELECT nom, prenom FROM client WHERE id = ?";
-        $statementClient = mysqli_prepare($conn, $queryClient);
-        mysqli_stmt_bind_param($statementClient, 'i', $client_id);
-        mysqli_stmt_execute($statementClient);
-        $resultClient = mysqli_stmt_get_result($statementClient);
-        $clientRow = mysqli_fetch_assoc($resultClient);
-
-        // Query for Caissier
-        $queryCaissier = "SELECT nom, prenom FROM caissier WHERE id = ?";
-        $statementCaissier = mysqli_prepare($conn, $queryCaissier);
-        mysqli_stmt_bind_param($statementCaissier, 'i', $caissier_id);
-        mysqli_stmt_execute($statementCaissier);
-        $resultCaissier = mysqli_stmt_get_result($statementCaissier);
-        $caissierRow = mysqli_fetch_assoc($resultCaissier);
-        
-        if (!$clientRow || !$caissierRow) {
-            header("Location: error.php");
-            exit();
-        }
-    }
+$totalSum = 0;
 
 
+// function to get bonlivraison by id from table bonlivraison with mysqli
+function getBonlivraisonById($conn, $bonlivraisonId)
+{
+    $sql ="SELECT bonlivraison.*, client.nom AS client_nom, caissier.nom AS caissier_nom
+    FROM bonlivraison
+    LEFT JOIN client ON bonlivraison.client_id = client.id
+    LEFT JOIN caissier ON bonlivraison.caissier_id = caissier.id
+    WHERE bonlivraison.id = $bonlivraisonId";
+
+    $result = $conn->query($sql);
+    $bonlivraisonRow = $result->fetch_assoc();
+    return $bonlivraisonRow;
 }
-mysqli_close($conn);
+// function to get bon details by bonlivraisonId
+function getBonDetailsByBonlivraisonId($conn, $bonlivraisonId)
+{    
+    $sql = "SELECT detail_bl.*, article.designation AS designation, article.prix_ht AS prix_ht, article.tva AS tva
+                            FROM detail_bl
+                            LEFT JOIN article ON detail_bl.article_id = article.id
+                            WHERE detail_bl.bl_id = $bonlivraisonId";
+    $result = $conn->query($sql);
+    return $result;
+}
+
+if (isset($_GET['id'])) {
+    $bonlivraisonId = $_GET['id'];
+    $bonLivraisonRow = getBonlivraisonById($conn, $bonlivraisonId);
 ?>
-
-
 <div class="container mt-4">
     <div class="bl">
         <center><h2 class="mb-4">Bon de livraison</h2></center>
         <input type="hidden" name="id" value="<?php echo "ID: " . $id; ?>">
-        <div class="mb-2">Client: <b><?php echo (isset($clientRow)) ? $clientRow['nom'] . ' ' . $clientRow['prenom'] : ''; ?></b></div>
-        <div class="mb-2">Caissier: <b><?php echo (isset($caissierRow)) ? $caissierRow['nom'] . ' ' . $caissierRow['prenom'] : ''; ?></b></div>
-        <div class="mb-2">Date: <b><?php echo ($bonLivraisonRow) ? $bonLivraisonRow['date'] : ''; ?></b></div>
-        <div class="mb-2">Régle: <b><?php echo ($bonLivraisonRow) ? $bonLivraisonRow['reglé'] : ''; ?></b></div>
+        <div class="mb-2">Client: <b><?php echo $bonLivraisonRow['client_nom']; ?></b></div>
+        <div class="mb-2">Caissier: <b><?php echo $bonLivraisonRow['caissier_nom']; ?></b></div>
+        <div class="mb-2">Date: <b></b><?php echo $bonLivraisonRow['date']; ?></div>
+        <div class="mb-2">Régle: <b></b><?php echo ($bonLivraisonRow['reglé'] == 1)?'Regle':'Non Regle'; ?></div>
         <hr class="my-4">
         <div class="table-responsive">
     <table class="table table-bordered">
         <thead>
             <tr>
                 <th>Identifiant</th>
-                <th>Famille</th>
-                <th>Article</th>
-                <th>Quantité</th>
-                <th>Prix + TVA</th>
+                <th scope="col">Designation</th>
+                <th scope="col">prix</th>
+                <th scope="col">Qte</th>
                 <th>TTC</th>
-                <th>Action</th>
             </tr>
-        </thead><?php
-
-        $totalTTC = 0;
-
-        while ($detailRow = ($resultDetailBl ? mysqli_fetch_assoc($resultDetailBl) : null)) {
-            $articleId = $detailRow['article_id'];
-
-            $queryArticle = "SELECT famille.famille, article.designation, article.prix, article.tva
-                             FROM article
-                             INNER JOIN famille ON article.famille_id = famille.id
-                             WHERE article.id = ?";
-
-            $statementArticle = mysqli_prepare($conn, $queryArticle);
-            mysqli_stmt_bind_param($statementArticle, 'i', $articleId);
-            mysqli_stmt_execute($statementArticle);
-            $resultArticle = mysqli_stmt_get_result($statementArticle);
-            $articleRow = mysqli_fetch_assoc($resultArticle);
-
-            if ($articleRow) {
-                $totalTTC += ($articleRow['prix'] + $articleRow['tva']) * $detailRow['qte'];
-
-                echo "<tr>";
-                echo "<td>" . $detailRow['id'] . "</td>";
-                echo "<td>" . $articleRow['famille'] . "</td>";
-                echo "<td>" . $articleRow['designation'] . "</td>";
-                echo "<td>" . $detailRow['qte'] . "</td>";
-                echo "<td>" . ($articleRow['prix'] + $articleRow['tva']) . "</td>";
-                echo "<td>" . ($articleRow['prix'] + $articleRow['tva']) * $detailRow['qte'] . "</td>";
-                echo "<td>";
-                echo '<button class="btn btn-danger"><a href="delete.php?id=' . $detailRow['id'] . '&table=detail_BL">Supprimer</a></button>';
-                echo '<button class="btn btn-warning"><a href="edit/editFacture.php?id=' . $detailRow['id'] . '&table=detail_BL &articleID=' . $detailRow['article_id'] . '&BL_id=' . $detailRow['bl_id'] . '&Qte=' . $detailRow['qte'] . '">Modifier</a></button>';
-                echo "</td>";
-                echo "</tr>";
+        </thead>
+        <tbody>
+            <?php
+            $result = getBonDetailsByBonlivraisonId($conn, $bonlivraisonId);
+            while ($row = $result->fetch_assoc()) {
+            ?>
+                <tr>
+                    <th scope="row"><?php echo($row["id"]);?></th>
+                    <td><?php echo($row["designation"]); ?></td>
+                    <td><?php echo(($row["prix_ht"] + $row["tva"])); ?></td>
+                    <td> <?php echo $row["qte"]?> </td>
+                    <td><?php  
+                    $rowTotal = ($row["prix_ht"] + $row["tva"]) * $row["qte"];
+                    $totalSum += $rowTotal;  
+                    echo (($row["prix_ht"] + $row["tva"]) * $row["qte"]); ?></td>
+                </tr>
+            <?php
             }
-        }
-        mysqli_stmt_close($statementArticle);
-        echo "</table>";
-
-        echo "<div class='text-center mt-3'><p class='h4'>Montant total : <span class='MT'>" . $totalTTC . "</span></p></div>";
-
         ?>
+        </table>
+
+        <div class='text-center mt-3'><p class='h4'>Montant total : <span class='MT'>
+            <?php echo $totalSum; ?>
+        </span></p></div>
+
 </div>
 
-<!-- ... (unchanged HTML code) ... -->
-
 <?php
+}
+
+mysqli_close($conn);
+
 include 'footer.php';
 ?>
